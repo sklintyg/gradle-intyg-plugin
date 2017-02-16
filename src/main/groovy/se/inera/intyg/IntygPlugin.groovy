329c822b90a9b05783eeea444537d28fe19c72f3
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2017-02-16 09:04:41 +0100

    DEVENV: Inkorporera kodkvalitets-verktyg i intygs-pluginen

--- src/main/groovy/se/inera/intyg/IntygPlugin.groovy
+++ src/main/groovy/se/inera/intyg/IntygPlugin.groovy
@@ -14,6 +14,8 @@ class IntygPlugin implements Plugin<Project> {
         applyCheckstyle(project)
         applyErrorprone(project)
         applyLicence(project)
+        applyJacoco(project)
+        applySonar(project)
 
         addGlobalTaskType(project, TagReleaseTask.class)
 
@@ -41,16 +43,20 @@ class IntygPlugin implements Plugin<Project> {
     }
 
     private void applyErrorprone(Project project) {
-        project.apply(plugin: 'net.ltgt.errorprone-base')
+        if (!project.hasProperty("useErrorprone") || project.ext.useErrorprone) {
+            project.apply(plugin: 'net.ltgt.errorprone-base')
 
-        // This makes sure only production code (not tests) is checked
-        if (project.hasProperty('codeQuality')) {
-            project.compileJava {
-                toolChain net.ltgt.gradle.errorprone.ErrorProneToolChain.create(project)
-                options.compilerArgs += ['-Xep:BoxedPrimitiveConstructor:ERROR', '-Xep:ClassCanBeStatic:ERROR', '-Xep:DefaultCharset:ERROR',
-                                         '-Xep:Finally:ERROR', '-Xep:FunctionalInterfaceClash:ERROR', '-Xep:ImmutableEnumChecker:ERROR',
-                                         '-Xep:MissingOverride:ERROR', '-Xep:NarrowingCompoundAssignment:ERROR', '-Xep:NonOverridingEquals:ERROR',
-                                         '-Xep:TypeParameterUnusedInFormals:ERROR', '-Xep:TypeParameterUnusedInFormals:ERROR']
+            // This makes sure only production code (not tests) is checked
+            if (project.hasProperty('codeQuality')) {
+                project.compileJava {
+                    toolChain net.ltgt.gradle.errorprone.ErrorProneToolChain.create(project)
+                    options.compilerArgs += ['-Xep:BoxedPrimitiveConstructor:ERROR', '-Xep:ClassCanBeStatic:ERROR', '-Xep:DefaultCharset:ERROR',
+                                             '-Xep:Finally:ERROR', '-Xep:FunctionalInterfaceClash:ERROR', '-Xep:ImmutableEnumChecker:ERROR',
+                                             '-Xep:MissingCasesInEnumSwitch:ERROR', '-Xep:MissingOverride:ERROR',
+                                             '-Xep:NarrowingCompoundAssignment:ERROR', '-Xep:NonOverridingEquals:ERROR',
+                                             '-Xep:TypeParameterUnusedInFormals:ERROR', '-Xep:TypeParameterUnusedInFormals:ERROR',
+                                             '-Xep:UnnecessaryDefaultInEnumSwitch:ERROR']
+                }
             }
         }
     }
@@ -65,8 +71,40 @@ class IntygPlugin implements Plugin<Project> {
             strictCheck = true
             header = null
             headerURI = IntygPlugin.class.getResource("/license/header.txt").toURI()
-            include("**/*.java")
+            include("src/**/*.java")
             mapping("java", "SLASHSTAR_STYLE")
+        }
+    }
+
+    private void applyJacoco(Project project) {
+        project.apply(plugin: 'jacoco')
+
+        project.jacoco {
+            toolVersion = "0.7.6.201602180812"
+        }
+
+        project.test {
+            jacoco {
+                destinationFile = project.file("${project.buildDir}/jacoco/test.exec")
+            }
+        }
+    }
+
+    private void applySonar(Project project) {
+        if (project == project.rootProject) {
+            project.apply(plugin: "org.sonarqube")
+
+            project.sonarqube {
+                properties {
+                    property "sonar.jacoco.reportPath", "${project.buildDir}/jacoco/test.exec"
+                    property "sonar.host.url", System.properties['sonarUrl'] ?: "https://build-inera.nordicmedtest.se/sonar"
+                    property "sonar.test.exclusions", "src/test/**"
+                    property "sonar.exclusions", ["**/stub/**", "**/test/**", "**/exception/**", "**/*Exception*.java", "**/*Fake*.java",
+                                                  "**/vendor/**", "**/*testability/**", "**/swagger-ui/**", "**/generatedSource/**",
+                                                  "**/templates.js"]
+                    property "sonar.javascript.lcov.reportPath", "${project.buildDir}/karma/merged_lcov.info"
+                }
+            }
         }
     }
 
