2c4ca4cbadee27e70385af49457b9f6ee92f70db
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2017-06-29 10:55:50 +0200

    DEVENV: nytt försök att få testreport att alltid köras

--- src/main/java/se/inera/intyg/SharedTestReportTask.java
+++ src/main/java/se/inera/intyg/SharedTestReportTask.java
@@ -1,5 +1,6 @@
 package se.inera.intyg;
 
+import org.gradle.api.tasks.testing.Test;
 import org.gradle.api.tasks.testing.TestReport;
 
 import java.util.concurrent.Callable;
@@ -9,7 +10,7 @@ public class SharedTestReportTask extends TestReport {
 	public SharedTestReportTask() {
 		setDescription("Create a consolidated test report for JUnit tests in all sub projects.");
 		setDestinationDir(getProject().file(getProject().getBuildDir() + "/reports/allTests"));
-		reportOn((Callable) () -> getProject().getTasksByName("test", true));
+		reportOn((Callable) () -> getProject().getTasksByName("test", true).stream().map( task -> ((Test)task).getBinResultsDir()));
 	}
 
 }
