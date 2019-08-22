3a00a60818d5b92e63445835ada02b5227c340b8
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2019-08-22 13:54:20 +0200

    BUILDENV: Uppdaterat urlen till repos så att vi cachar jcenter dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,5 +62,6 @@ dependencies {
 repositories {
     maven("https://plugins.gradle.org/m2/")
     mavenLocal()
-    mavenCentral()
+    maven("https://build-inera.nordicmedtest.se/nexus/repository/public/")
+    jcenter()
 }
